8190acfc8fa3654af72f77bc04f9fa5d6d361a6b
Pingde Gu <peterouwen@gmail.com>
2020-03-12 01:19:50 -0700

    Content View
    Adding a content view, including the layout, and alternations to firebase object

--- app/src/main/java/com/example/oneclicktrader/phoneItem.java
+++ app/src/main/java/com/example/oneclicktrader/phoneItem.java
@@ -1,13 +1,17 @@
 package com.example.oneclicktrader;
 
 public class phoneItem {
-    public String brand, condition;
-    public double price, storageSize;
+    public String brand, model, condition, color;
+    public double price;
+    public int RAMSize, storageSize;
 
-    public phoneItem(String brand, String condition, double price, double storageSize) {
+    public phoneItem(String brand, String model, String condition, String color, double price, int RAMSize, int storageSize) {
         this.brand = brand;
+        this.model = model;
         this.condition = condition;
+        this.color = color;
         this.price = price;
+        this.RAMSize = RAMSize;
         this.storageSize = storageSize;
     }
 }
