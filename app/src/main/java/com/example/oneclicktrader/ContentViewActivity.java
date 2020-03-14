82f8508580cc989eddbdb45233a3e34604e3bd95
NathanSu <sulain0304@gmail.com>
2020-03-13 17:02:07 -0700

    some new

--- app/src/main/java/com/example/oneclicktrader/ContentViewActivity.java
+++ app/src/main/java/com/example/oneclicktrader/ContentViewActivity.java
@@ -14,5 +14,12 @@ public class ContentViewActivity extends AppCompatActivity {
         setContentView(R.layout.content_view);
 //        model = (TextView) findViewById(R.id.cell_image);
 
+<<<<<<< Updated upstream
+=======
+    public void trigerAlert() {
+        String msg = "May only offer price no lower than $" + phoneItem.getPrice()*0.8 + " (80% of original price), and no higher than original price.\n";
+        AlertTable a = new AlertTable(msg);
+        a.show(getSupportFragmentManager(), "Offer Price");
+>>>>>>> Stashed changes
     }
 }
